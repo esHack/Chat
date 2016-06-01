ddb8d287477aebd11a2f5f3b85e694cb58e175ab
esHack <eswaryaga@gmail.com>
2016-06-01 17:05:16 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
  
     var messages = [];
-    var socket = io.connect('http://eshx.azurewebsites.net/');
+    var socket = io.connect('https://esh-x.herokuapp.com/');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
